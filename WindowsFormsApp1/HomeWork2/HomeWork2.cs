241b816147466ce02d83ee61a1a6005a9e488de4
Ggeroy11 <elnar1995@mail.ru>
2017-10-03 13:58:10 +0600

    home work1 done

---
+++ WindowsFormsApp1/HomeWork2/HomeWork2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork2
+{
+    public partial class HomeWork2 : Form
+    {
+        public HomeWork2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Game();
+
+        }
+        
+
+        private void Game()
+        {
+            while (true) {
+                
+                DialogResult result = MessageBox.Show("Загадайте число от 1 до 2000", "Игра началась", MessageBoxButtons.YesNo);
+                Random random = new Random();
+                DialogResult tryAgain=0;
+
+                do {
+                    DialogResult win = MessageBox.Show($"Ваше число {random.Next(1, 2000)}" + $"?", "", MessageBoxButtons.YesNo);
+                    if (win == DialogResult.Yes) {
+                        MessageBox.Show("Мы угадали число!!!");
+                        tryAgain=MessageBox.Show("Хотите поиграть еще","еще одна попытка",MessageBoxButtons.YesNo);
+                        if (tryAgain == DialogResult.No) { 
+                            this.Close();
+                        }
+                    }
+                } while (tryAgain != DialogResult.Yes);
+
+            }
+            
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/HomeWork1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class HomeWork1 : Form
+    {
+        public HomeWork1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string [] caption= {"Фамилия","Имя","Возраст"};
+            string[] resume = { "Уважанов", "Елнар", "22" };
+            int count = 0;
+            for (int i=0;i<caption.Length;i++) {
+                MessageBox.Show(resume[i],caption[i],MessageBoxButtons.OK);                
+            }
+            foreach (string a in resume) {
+                count += a.Length;
+            }
+            MessageBox.Show((count / resume.Length).ToString(), "Среднее число символов на странице",
+               MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
